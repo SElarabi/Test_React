480924a43b8f3409348aa74d41efe86cf716a894
Eddie Larabi <117444690+SElarabi@users.noreply.github.com>
2023-11-13 10:18:41 -0500

    initial start using secret from dotenv

--- test7.js
+++ test7.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const varTest = process.env.SECRET_KEY;
-
+console.log('SECRET VARIABLE FROM .ENV ', varTest);
 const button = document.getElementById('myButton');
 
 // Add a click event listener to the button
