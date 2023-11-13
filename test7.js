e7bcc61c60c5871347a8126d49a4b650f1522416
Eddie Larabi <117444690+SElarabi@users.noreply.github.com>
2023-11-13 10:01:13 -0500

    add index.html

--- test7.js
+++ test7.js
@@ -2,4 +2,10 @@
 
 require('dotenv').config();
 const varTest = process.env.SECRET_KEY;
-console.log(varTest);
+
+const button = document.getElementById('myButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', function () {
+	console.log(varTest);
+});
